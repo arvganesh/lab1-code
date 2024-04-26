f0489e3c62976949d9801eece48da774a48e1214
arvgan <arvgan@node0.lab1.utcs-aos-s24-pg0.utah.cloudlab.us>
2024-04-26 14:50:35 -0600

    added compete for memory code

--- memory_experiments.cpp
+++ memory_experiments.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <sys/sysinfo.h>
 #include <sys/resource.h>
 #include <unistd.h>
 #include <string>
@@ -349,7 +350,6 @@ void runTrials(int numTrials) {
 
 void parseArgs(int argc, char* argv[]) {
    std::vector<std::string> args(argv + 1, argv + argc);
-
    for (auto it = args.begin(); it != args.end(); ++it) {
       if (*it == "random") {
          opt_random_access = 1;
@@ -365,6 +365,8 @@ void parseArgs(int argc, char* argv[]) {
          use_msync = 1;
       } else if (*it == "debug") {
          debug = 1;
+      }  else if (*it == "fork") {
+         should_fork = 1;
       }
    }
 }
@@ -380,8 +382,57 @@ void lockToSingleProcessor() {
    } 
 }
 
+int get_mem_size() {
+   struct sysinfo info;
+   CHECK_ERR(sysinfo(&info), "sysinfo");
+   return info.totalram;
+}
+
+int compete_for_memory() {
+   long mem_size = get_mem_size();
+   int page_sz = sysconf(_SC_PAGE_SIZE);
+   printf("Total memsize is %3.2f GBs\n", (double)mem_size/(1024*1024*1024));
+   fflush(stdout);
+   char* p = mmap(NULL, mem_size, PROT_READ | PROT_WRITE,
+                  MAP_NORESERVE|MAP_PRIVATE|MAP_ANONYMOUS, -1, (off_t) 0);
+   if (p == MAP_FAILED)
+      perror("Failed anon MMAP competition");
+
+   int i = 0;
+   while(1) {
+      volatile char *a;
+      long r = simplerand() % (mem_size/page_sz);
+      char c;
+      if( i >= mem_size/page_sz ) {
+         i = 0;
+      }
+      // One read and write per page
+      //a = p + i * page_sz; // sequential access
+      a = p + r * page_sz;
+      c += *a;
+      if((i%8) == 0) {
+         *a = 1;
+      }
+      i++;
+   }
+   return 0;
+}
+
+void handle_fork() {
+   pid_t proc = fork();
+   CHECK_ERR(proc, "fork");
+   if (proc == 0) {
+      compete_for_memory();
+      exit(0);
+   }
+}
+
+int should_fork = 0;
 int main(int argc, char* argv[]) {
    parseArgs(argc, argv);
+   if (should_fork) {
+      handle_fork();
+   }
    lockToSingleProcessor();
    runTrials(1);
    return 0;
